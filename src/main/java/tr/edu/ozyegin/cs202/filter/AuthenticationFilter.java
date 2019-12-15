a393dc762f38f0b4c9fcd92cba16a407ad8a04d9
Zehra Ozgenlik <zehra.ozgenlik@ozu.edu.tr>
2019-12-15 21:39:39 +0300

    Redirect to login if user is not logged in

---
+++ src/main/java/tr/edu/ozyegin/cs202/filter/AuthenticationFilter.java
@@ -0,0 +1,37 @@
+package tr.edu.ozyegin.cs202.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+        String loginURI = request.getContextPath() + "/login";
+
+        boolean loggedIn = session != null && session.getAttribute("currentUser") != null;
+        boolean loginRequest = request.getRequestURI().equals(loginURI);
+
+        if (loggedIn || loginRequest) {
+            chain.doFilter(request, response);
+        } else {
+            response.sendRedirect(loginURI);
+        }
+    }
+}
